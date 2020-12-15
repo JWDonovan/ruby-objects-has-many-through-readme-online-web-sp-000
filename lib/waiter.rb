9886671a189aa980d7dc05afca983d4f333b5b6c
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 23:54:55 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -24,4 +24,12 @@ class Waiter
   def best_tipper
     meals.sort_by(meal.tip)
   end
+
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+  
+    best_tipped_meal.customer
+  end
 end
